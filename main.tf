6bf1afa35aba3c1de2dcc476fad3fc450ccbfd71
rohithmamidi <rohithmamidi123@gmail.com>
2025-10-17 01:44:23 +0530

    test2

--- main.tf
+++ main.tf
@@ -2,23 +2,22 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# Generate unique suffix for S3 bucket
 resource "random_id" "suffix" {
   byte_length = 4
 }
 
-# S3 bucket to store the app
+# S3 bucket without ACL
 resource "aws_s3_bucket" "app_bucket" {
-  bucket = "capstone-task3-app-${random_id.suffix.hex}"
-  acl    = "public-read"
+  bucket        = "capstone-task3-app-${random_id.suffix.hex}"
+  force_destroy = true   # allows deletion even if non-empty
 }
 
-# Upload index.html to S3
-resource "aws_s3_bucket_object" "app_file" {
+# Upload index.html and make it public
+resource "aws_s3_object" "app_file" {
   bucket = aws_s3_bucket.app_bucket.id
   key    = "index.html"
   source = "index.html"
-  acl    = "public-read"
+  acl    = "public-read"  # only on the object
 }
 
 # Security group to allow HTTP
@@ -47,7 +46,6 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
   security_groups = [aws_security_group.allow_http.name]
 
-  # User Data to fetch app from S3 and start Python HTTP server
   user_data = <<-EOF
               #!/bin/bash
               yum install -y python3 awscli
@@ -61,12 +59,11 @@ resource "aws_instance" "app_server" {
   }
 }
 
-# Output EC2 public IP
+# Outputs
 output "ec2_public_ip" {
   value = aws_instance.app_server.public_ip
 }
 
-# Output S3 bucket name
 output "s3_bucket" {
   value = aws_s3_bucket.app_bucket.bucket
 }
